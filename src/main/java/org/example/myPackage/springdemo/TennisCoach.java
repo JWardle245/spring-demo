61107bc4f08dc2845ebf287261f60ad1314bde02
JWardle245 <74054752+JWardle245@users.noreply.github.com>
2020-12-22 12:30:42 +0000

    Add files via upload
    Added new class: TennisCoach

---
+++ src/main/java/org/example/myPackage/springdemo/TennisCoach.java
@@ -0,0 +1,9 @@
+package org.example.myPackage.springdemo;
+
+public class TennisCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout(){
+        return "Practice backhand for 20 minutes";
+    }
+}
